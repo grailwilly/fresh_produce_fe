c08609a96209a48e12e77af8d5426b6ad2c39ee1
johndv <johndominicvillalino@outlook.com>
2022-08-30 19:06:44 +1000

    remove focus on cart button

--- src/components/pages/products/BuyABox.js
+++ src/components/pages/products/BuyABox.js
@@ -63,7 +63,7 @@ const BuyABox = ({ productItems, productBox, cardData, increaseQty, decreaseQty,
                         </div>
                         <span className="mt-5 text-xs text-dk-white">{`10 ${e.qty_measurement} / box`}</span>
 
-                        <button className=" bg-teal hover:bg-dark-green flex items-center justify-center w-full px-2 py-2 mt-4 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform rounded-md  focus:outline-none focus:bg-gray-700">
+                        <button className=" bg-teal hover:bg-dark-green flex items-center justify-center w-full px-2 py-2 mt-4 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform rounded-md  ">
                           <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 mx-1" viewBox="0 0 20 20" fill="currentColor">
                             <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
                           </svg>
