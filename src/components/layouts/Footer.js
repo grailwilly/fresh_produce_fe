78591ca7b0955f6308489d1d42205e65ee70a783
grailwilly <grailjwi11y@gmail.com>
2022-09-02 10:59:18 +0800

    Change link

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -88,7 +88,7 @@ export const Footer = () => {
                     </a>
                   </div>
                   <div>
-                    <a href="https://www.instagram.com/farmersfineproduce/">
+                    <a href="https://www.instagram.com/farmersvalleyph/">
                       <svg aria-label="Instagram" xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="#718096" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="feather feather-instagram">
                         <rect x={2} y={2} width={20} height={20} rx={5} ry={5} />
                         <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
