f6d3dfc5ff9936ee128614cad5e1e677c1ab2fec
grailwilly <grailjwi11y@gmail.com>
2022-09-05 20:43:32 +0800

    Add header and footer

--- src/components/pages/Farmers.js
+++ src/components/pages/Farmers.js
@@ -1,51 +1,16 @@
 import React from 'react';
+import { Footer } from "../layouts/Footer";
 
 const Farmers = () => {
   return (
     <>
-      <div id="carouselExampleControls" className="carousel slide relative" data-bs-ride="carousel">
-        <div className="carousel-inner relative w-full overflow-hidden">
-          <div className="carousel-item active relative float-left w-full">
-            <img
-              src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp"
-              className="block w-full"
-              alt="Wild Landscape"
-            />
-          </div>
-          <div className="carousel-item relative float-left w-full">
-            <img
-              src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp"
-              className="block w-full"
-              alt="Camera"
-            />
-          </div>
-          <div className="carousel-item relative float-left w-full">
-            <img
-              src="https://mdbcdn.b-cdn.net/img/new/slides/043.webp"
-              className="block w-full"
-              alt="Exotic Fruits"
-            />
-          </div>
+      <header
+        className="w-full h-[70vh] bg-[url('https://images.unsplash.com/photo-1609554259810-ad331c1a9519?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80')] bg-cover bg-center flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center">
+            <h1 className="text-center text-5xl text-white font-bold drop-shadow-lg">Meet the Farmers</h1>
         </div>
-        <button
-          className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
-          type="button"
-          data-bs-target="#carouselExampleControls"
-          data-bs-slide="prev"
-        >
-          <span className="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
-          type="button"
-          data-bs-target="#carouselExampleControls"
-          data-bs-slide="next"
-        >
-          <span className="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-      </div>
+      </header>
+      <Footer />
     </>
   )
 }
