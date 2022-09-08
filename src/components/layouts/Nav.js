fcc7adf1605eaf014d95ac79aefebb0e86d92670
grailwilly <grailjwi11y@gmail.com>
2022-09-08 13:40:15 +0800

    Add logo navbar

--- src/components/layouts/Nav.js
+++ src/components/layouts/Nav.js
@@ -6,7 +6,6 @@ import { connect } from 'react-redux';
 import { Menu, Transition } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 
-
 const Nav = ({ user, signOut }) => {
 
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -67,24 +66,8 @@ const Nav = ({ user, signOut }) => {
           title="Company"
           className="inline-flex items-center lg:mx-auto"
         >
-          <svg
-            className="w-8 text-deep-purple-accent-400"
-            viewBox="0 0 24 24"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeMiterlimit="10"
-            stroke="currentColor"
-            fill="none"
-          >
-            <rect x="3" y="1" width="7" height="12" />
-            <rect x="3" y="17" width="7" height="6" />
-            <rect x="14" y="1" width="7" height="6" />
-            <rect x="14" y="11" width="7" height="12" />
-          </svg>
-          <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
-            Farmer's Valley Market
-          </span>
+          
+          <img src={require('../../assets/logo_nav.png')} />
         </Link>
         <ul className="flex items-center hidden ml-auto space-x-8 lg:flex">
 
