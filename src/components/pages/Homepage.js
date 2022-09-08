897b7da18b7d45f1da6b5fff827a10221e56290a
grailwilly <grailjwi11y@gmail.com>
2022-09-08 16:02:29 +0800

    Add carousel images

--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -1,12 +1,15 @@
 
+import Carousel from "../layouts/Carousel";
 import { Footer } from "../layouts/Footer";
+
 const Homepage = () => {
 
   return (
     <>
-      <div className="pt-32 lg:flex items-center relative z-10 container mx-auto">
+    
+      <div className="pt-30 lg:flex items-center relative z-10 container mx-auto">
         <div className="w-full lg:w-1/2 h-full lg:pr-10 xl:pr-0">
-          <img tabIndex="0" role="img" aria-label="people smiling" className="mx-auto" src="https://cdn.tuk.dev/assets/templates/weCare/hero2-left.png"  alt="people smiling"/>
+          <Carousel />
         </div>
         <div  role="contentinfo"  className="w-full lg:w-1/2 h-full">
           <p tabIndex="0" className="uppercase text-2xl mb-4">From Farms To Our Customers</p>
