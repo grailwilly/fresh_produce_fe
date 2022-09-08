d93d572a8661cd820e7d099ad8ebf6b791386e32
grailwilly <grailjwi11y@gmail.com>
2022-09-08 12:49:50 +0800

    Add dropdown avatar

--- src/components/layouts/Nav.js
+++ src/components/layouts/Nav.js
@@ -1,8 +1,10 @@
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
 import CartIcon from './cart/CartIcon';
 import { signOut } from '../actions/user';
 import { connect } from 'react-redux';
+import { Menu, Transition } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
 
 
 const Nav = ({ user, signOut }) => {
@@ -87,7 +89,99 @@ const Nav = ({ user, signOut }) => {
         <ul className="flex items-center hidden ml-auto space-x-8 lg:flex">
 
           {user.action && <CartIcon></CartIcon>}
-          {user.action && <button onClick={signOut}>Logout</button>}
+          {user.action && 
+            <li>
+              <Menu as="div" className="relative inline-block text-left">
+              <div>
+                <Menu.Button className="inline-flex w-full justify-center">
+                  <img class="p-1 w-10 h-10 rounded-full ring-2 ring-gray-300 dark:ring-gray-500" src="https://img.icons8.com/doodle/48/000000/user-male-skin-type-5.png" alt="avatar" />
+                  <ChevronDownIcon className="-mr-1 ml-2 mt-2 h-5 w-5" aria-hidden="true" />
+                </Menu.Button>
+              </div>
+
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                  <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <div className="py-1">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={
+                              (active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                              'block px-4 py-2 text-sm')
+                            }
+                          >
+                            User Name/Email
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <hr />
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={
+                              (active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                              'block px-4 py-2 text-sm')
+                            }
+                          >
+                            Account settings
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={
+                              (active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                              'block px-4 py-2 text-sm')
+                            }
+                          >
+                            Profile
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={
+                              (active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                              'block px-4 py-2 text-sm')
+                            }
+                          >
+                            Dashboard
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item onClick={signOut}>
+                        {({ active }) => (
+                          <button
+                            type="submit"
+                            className={
+                              (active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                              'block w-full px-4 py-2 text-left text-sm')
+                            }
+                          >
+                            Sign out
+                          </button>
+                        )}
+                      </Menu.Item>
+                    </div>
+                  </Menu.Items>
+                </Transition>
+              </Menu>
+            </li>
+          }
           {!user.action &&
             <li>
               <Link
