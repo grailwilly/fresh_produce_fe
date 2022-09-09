702dfd442208187c534eea6464377d97568309d7
grailwilly <grailjwi11y@gmail.com>
2022-09-09 16:55:08 +0800

    breakpoints not working

--- src/components/pages/Farmers.js
+++ src/components/pages/Farmers.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Footer } from "../layouts/Footer";
+import farmerBanner from "../../assets/farmer_banner.png";
 
 const Farmers = () => {
   return (
     <>
       <header
-        className="w-full h-[70vh] bg-[url('https://images.unsplash.com/photo-1609554259810-ad331c1a9519?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80')] bg-cover bg-center flex justify-center items-center">
+        className="w-full h-[70vh] flex justify-center items-center" style={{ backgroundImage: `url(${farmerBanner})`, backgroundPosition: 'center',
+        backgroundSize: 'cover'}}>
         <div className="flex flex-col justify-center items-center">
-            <h1 className="text-center text-5xl text-white font-bold drop-shadow-lg">Meet the Farmers</h1>
+          <h1 className="text-center text-5xl text-white font-bold drop-shadow-lg">Meet the Farmers</h1>
         </div>
       </header>
       <section className="container px-5 py-2 mx-auto lg:pt-12 lg:px-32">
