1169c6002caa6111dbdec8f66861ca7f2a80fc0b
grailwilly <grailjwi11y@gmail.com>
2022-09-09 22:49:53 +0800

    class to className

--- src/components/layouts/Sidebar.js
+++ src/components/layouts/Sidebar.js
@@ -106,7 +106,7 @@ const Sidebar = ({ signOut }) => {
                         >
                           <svg 
                             xmlns="http://www.w3.org/2000/svg" 
-                            class="icon icon-tabler icon-tabler-pencil" 
+                            className="icon icon-tabler icon-tabler-pencil" 
                             width="20" height="20" 
                             viewBox="0 0 24 24" 
                             strokeWidth={2} 
