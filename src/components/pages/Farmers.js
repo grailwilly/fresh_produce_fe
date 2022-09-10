ae22137e9cb997486bcbf4d6f1cb102103161dc8
grailwilly <grailjwi11y@gmail.com>
2022-09-10 09:26:07 +0800

    Add fix align cards

--- src/components/pages/Farmers.js
+++ src/components/pages/Farmers.js
@@ -19,9 +19,6 @@ const Farmers = ({ farm, getAllFarmers }) => {
     setAllFarmers(farm)
   }, [farm])
 
-
-
-
   return (
     <>
       <header
@@ -32,25 +29,24 @@ const Farmers = ({ farm, getAllFarmers }) => {
         </div>
       </header>
       <section className="container px-5 py-2 mx-auto lg:pt-12 lg:px-32">
-
+      <div className="flex flex-wrap -m-1 md:-m-2">
         {allFarmers.map(e => {
           return (
-            <div className="flex flex-wrap -m-1 md:-m-2">
-              <div className="flex flex-wrap w-1/3">
-                <div className="group relative w-96">
-                  <img className="w-full object-cover"
-                    src="https://images.unsplash.com/photo-1589923188900-85dae523342b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
-                  <div
-                    className="absolute top-0 left-0 w-full h-0 flex flex-col justify-center items-center bg-teal opacity-0 group-hover:h-full group-hover:opacity-100 duration-500">
-                    <h1 className="text-2xl text-white">{`${e.first_name.toUpperCase()} ${e.last_name.toUpperCase()}`}</h1>
-                    <a className="mt-5 px-8 py-3 rounded-full bg-gold duration-300" href="#">View More</a>
-                  </div>
+            <div className="flex flex-wrap w-1/3">
+              <div className="group relative w-96">
+                <img className="w-full object-cover"
+                  src="https://images.unsplash.com/photo-1564622598577-74a9aeff6b37?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" />
+                <div
+                  className="absolute top-0 left-0 w-full h-0 flex flex-col justify-center items-center bg-teal opacity-0 group-hover:h-full group-hover:opacity-100 duration-500">
+                  <h1 className="text-2xl text-white">{`${e.first_name.toUpperCase()} ${e.last_name.toUpperCase()}`}</h1>
+                  <a className="mt-5 px-8 py-3 rounded-full bg-gold duration-300" href="#">View More</a>
                 </div>
               </div>
             </div>
-          )
-        })
+            )
+          })
         }
+        </div>
       </section>
       <Footer />
     </>
